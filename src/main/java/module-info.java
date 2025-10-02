00ca44f591a366e09405be77a0d16e4cb86eeb25
natthapoomsaengkaew <natthapoomsaengkaew@gmail.com>
2025-10-02 10:08:38 +0700

    update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module se233.chapter6 {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.apache.logging.log4j;
+    requires org.apache.logging.log4j.core;
 
 
     opens se233.chapter6 to javafx.fxml;
